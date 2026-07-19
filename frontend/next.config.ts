f20a539a1041bcb0a8304728fbdc9768a1c0527f
MK340 Developer <dev@mk340.com>
2026-07-20 04:32:55 +0700

    feat: add next.config.ts configuration file to frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    // ใช้ API_URL จาก Environment Variable ถ้ามี (สำหรับเว็บจริง)
+    // ใช้ API_URL จาก Environment Variable 
     // ถ้าไม่มีจะใช้ http://localhost:3001 สำหรับทดสอบในเครื่อง
     const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
-    
+
     return [
       {
         source: "/api/:path*",
